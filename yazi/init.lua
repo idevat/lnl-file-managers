b350ddb32406517a94edb668b4b778af7a288191
Ivan Devat <idevat@redhat.com>
2025-04-27 14:39:23 +0200

    add bookmarks

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,3 +1,13 @@
 require("session"):setup {
   sync_yanked = true,
 }
+
+require("bookmarks"):setup {
+  last_directory = {
+    enable = true,
+    persist = true,
+    mode = "jump",
+  },
+  persist = "all", -- vim: only in upper case (A-Z) are persistent
+  custom_desc_input = false,
+}
